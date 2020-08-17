a77a38d20add4e258ce0de7f00d88c1a62e1b695
Kevin Fridman <kevin.fridman@yh.nackademin.se>
2020-08-17 15:25:13 +0200

    fixed update function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const dataStore = require('nedb')
 const app = express()
 let db = new dataStore({filename: './database.db', autoload: true});
 
+//LÄGGER TILL NYA BLOGGINLÄGG I DATABASEN
 app.post('/create/:title/:content', (req, res) => {
     var blogPost = { title: req.params.title, content: req.params.content };
     db.insert(blogPost, (err, newDoc)  => { 
@@ -11,18 +12,28 @@ app.post('/create/:title/:content', (req, res) => {
     });
 })
 
+//LÄSER IN EN ENSKILT BLOGGINLÄGG
 app.get('/read/:id', (req, res) => {
   db.find({ _id: req.params.id }, function (err, docs) {
-    
-    // docs is an array containing documents Mars, Earth, Jupiter
-    // If no document is found, docs is equal to []
     res.send(docs)
   });
   
 })
 
-app.get('/update', (req, res) => {
-    res.send('Nothing here!')
+//UPPDATERAR BEFINTLIGA INLÄGG
+app.put('/update/:id/:title/:content', (req, res) => {
+ 
+  db.find({ _id: req.params.id },  (err, docs) => {
+    
+    db.update({_id:req.params.id}, {title: req.params.title , content: req.params.content }, (err, updateDoc) => {
+    
+     res.send(updateDoc)
+      
+  });
+  
+});
+
+    //res.send(updateDoc);
 })
 
 app.get('/delete', (req, res) => {
